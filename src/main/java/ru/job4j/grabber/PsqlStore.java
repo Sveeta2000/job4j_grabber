249824953686bbcfda712558e6886cbc8e418186
sveet <sveetlana2000@mail.ru>
2023-10-31 18:17:23 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,124 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        HabrCareerParse parse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> posts = parse.list("https://career.habr.com/vacancies/java_developer?page=");
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("psql_store.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            try (PsqlStore store = new PsqlStore(config)) {
+                for (Post post : posts) {
+                    store.save(post);
+                }
+                List<Post> retrievedPosts = store.getAll();
+                retrievedPosts.forEach(System.out::println);
+                Post post = store.findById(18);
+                System.out.println(post);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement  = cnn.prepareStatement(
+                "INSERT INTO post(name, text, link, created) VALUES (?, ?, ?, ?) ON CONFLICT (link) DO NOTHING",
+                Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.executeUpdate();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                while (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> postList = new ArrayList<>();
+        try (PreparedStatement statement  = cnn.prepareStatement(
+                "SELECT * FROM post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    postList.add(createPost(resultSet));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return postList;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = new Post();
+        try (PreparedStatement statement  = cnn.prepareStatement(
+                "SELECT * FROM post WHERE id = ?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                    post.setTitle(resultSet.getString("name"));
+                    post.setLink(resultSet.getString("link"));
+                    post.setDescription(resultSet.getString("text"));
+                    post.setCreated(resultSet.getTimestamp("created").toLocalDateTime());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    private Post createPost(ResultSet resultSet) {
+        try {
+            return new Post(resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("link"),
+                    resultSet.getString("text"),
+                    resultSet.getTimestamp("created").toLocalDateTime());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
